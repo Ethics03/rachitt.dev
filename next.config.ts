54645d276c546d28bac73e170421d3d4efe86121
Ethics03 <honey032004@gmail.com>
2025-10-21 20:56:56 +0530

    feat: configured mdx and adding configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+const withMDX = require("@next/mdx")();
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions`` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
 };
 
-export default nextConfig;
+module.exports = withMDX(nextConfig);
