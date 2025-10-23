566f5375215f1eaf632c6d57ed69277fdb40bb70
Ethics03 <honey032004@gmail.com>
2025-10-23 21:45:52 +0530

    ehh

--- src/app/lib/work.ts
+++ src/app/lib/work.ts
@@ -1,4 +1,4 @@
 
 export const work = [
-    { company: "Fordsmen", role: "Backend Engineer Intern", href: "https://fordsmen.com", description: "Worked as a backend developer, implemented custom auth system, caching, backend logic, testing and deployment" }
+    { company: "Fordsmen", role: "Backend Engineer Intern", href: "https://fordsmen.com", description: "worked as a backend developer, implemented custom auth system, caching, backend logic, testing and deployment" }
   ];
