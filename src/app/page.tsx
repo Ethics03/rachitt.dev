58e6d88c4f763ec62fdbff10f95966a734ac9370
Ethics03 <honey032004@gmail.com>
2025-10-23 21:16:06 +0530

    refactor: added new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,20 @@
-"use client";
-import { motion } from "framer-motion";
-import Link from "next/link";
-import ScrambleText from "./components/ScrambleText";
+import HeroSection from "./components/heroSection";
+import WorkSection from "./components/workSection";
+import ProjectsSection from "./components/projectSection";
+import BlogSection from "./components/blogSection";
+import LinkSection from "./components/linkSection";
+
 
 export default function Home() {
-  const menuItems = [
-    { label: "About", href: "/about" },
-    { label: "Projects", href: "/projects" },
-    { label: "Work", href: "/work" },
-    { label: "Blogs", href: "/blog" },
-  ];
+  return (
+    <div className="flex items-center pt-10">
+      <div className="max-w-5xl">
+        <HeroSection />
+        <WorkSection />
+        <ProjectsSection />
+        <BlogSection />
+        <LinkSection/>
 
-    return (
-  <div className="h-full flex items-start md:items-center pt-20 md:pt-30 w-full">
-  <div className="flex flex-col gap-10 w-full max-w-3xl mx-auto px-4 md:px-0 items-start md:items-center">
-        {menuItems.map((item, index) => (
-          <Link key={item.label} href={item.href} passHref>
-            <motion.div
-              whileHover={{ x: 10 }}
-              transition={{ type: "spring", stiffness: 300, damping: 20 }}
-              className="text-3xl md:text-5xl font-bold text-white/80 hover:text-white transition-colors cursor-pointer"
-            >
-              <ScrambleText text={item.label} delay={index * 200} />
-            </motion.div>
-          </Link>
-        ))}
-
-        <div className="mt-8 text-base md:text-lg text-white/60">
-          <ScrambleText text="wanna talk?" delay={800} className="block mb-2" />
-          <a
-            href="mailto:hello@rachitt.dev"
-            className="text-white/80 hover:text-white transition-colors"
-          >
-            <ScrambleText text="hello@rachitt.dev" delay={1000} />
-          </a>
-        </div>
       </div>
     </div>
   );
